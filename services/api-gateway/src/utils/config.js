4f7286fb902638e893d7fdae0ba91ff3c1f4da5d
alvarogall <varosg@proton.me>
2025-12-18 23:08:06 +0100

    config

--- services/api-gateway/src/utils/config.js
+++ services/api-gateway/src/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  PORT: process.env.GATEWAY_PORT,
+  PORT: process.env.PORT || process.env.GATEWAY_PORT || 10000,
   CALENDAR_SERVICE_URL: process.env.CALENDAR_SERVICE_URL,
   EVENT_SERVICE_URL: process.env.EVENT_SERVICE_URL,
   COMMENT_SERVICE_URL: process.env.COMMENT_SERVICE_URL,
